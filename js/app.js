3531060eaf6537a225efe3f231d566109ea792dd
adriancgarcia <adriancgarcia@gmail.com>
2023-01-21 21:53:58 -0600

    tamagotchi class completed

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('Dino Tamagotchi')
+
+class Tamagotchi {
+    constructor (name, Hunger, Sleepiness, Boredom, Age ) {
+        // this.name = 'Arlo';
+        this.Hunger = 10;
+        this.Sleepiness = 20;
+        this.Boredom = 20;
+        this.Age = 5;
+    }
+// giveName() {
+//     console.log(`My name is ${this.name}`)
+// }
+}
+
+// const nombre = new Tamagotchi ('');
+// nombre.giveName();
